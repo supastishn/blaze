485eb56098c4a4cb672784a0d53ba37e205a9d10
supastishn <omareeto2012@hotmail.com>
2025-07-27 15:50:49 +0000

    feat: add CLI entry point and basic compiler stub

---
+++ src/cli/index.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { compile } from '../compiler';
+import fs from 'fs/promises';
+
+const cli = new Command();
+
+cli
+  .name('ts2cpp')
+  .description('TypeScript to C++ Compiler')
+  .version('1.0.0');
+
+cli
+  .command('compile <source>')
+  .description('Compile TypeScript to C++')
+  .option('-o, --output <file>', 'Output file')
+  .action(async (source, {output}) => {
+    try {
+      const code = await fs.readFile(source, 'utf-8');
+      const cppCode = compile(code);
+      if (output) {
+        await fs.writeFile(output, cppCode);
+      } else {
+        console.log(cppCode);
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+
+cli.parse(process.argv);
---
+++ src/compiler/index.ts
@@ -0,0 +1,6 @@
+import { Parser } from '../parser/parser';
+
+export const compile = (source: string): string => {
+  // TODO: Implement compilation logic
+  return '// C++ code output';
+};
